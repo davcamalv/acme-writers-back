1d2f1c85a2675b0949cb4dd95a68c4acfc9d2706
davcamalv <dcamalv@gmail.com>
2020-08-04 19:32:50 +0200

    fix(opinion): añadido id del usuario en el dto devuelto

--- app/Dtos/BasicUserDto.php
+++ app/Dtos/BasicUserDto.php
@@ -4,8 +4,9 @@ namespace App\Dtos;
 
 final class BasicUserDto{
 
-    public function __construct(?string $name, ?string $photo)
+    public function __construct(?int $id, ?string $name, ?string $photo)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->photo = $photo;
     }
@@ -26,4 +27,12 @@ final class BasicUserDto{
             return $this->photo;
     }
 
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+            return $this->id;
+    }
 }
